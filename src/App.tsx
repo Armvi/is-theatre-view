3b0bc9418a1f8c29af995b7fe0810c69a9cf379b
globalskye <43073544+globalskye@users.noreply.github.com>
2022-11-28 07:14:16 +0300

    akilart noob

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { AdminComponent } from './admin/admin';
+
+import { Routes, Route, Link, useRoutes } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import './App.css';
 
+
 function App() {
-  return (
-    <AdminComponent></AdminComponent>
-  );
+  let routs = useRoutes(
+    [
+      {path:"/login",element:<Login/>},
+      {path:"/register",element:<Register/>},
+    ]
+  )
+  return routs
+ 
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AdminComponent } from './admin/admin';
+import{ BrowserRouter as Router, Route} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <AdminComponent></AdminComponent>
+    <Router>
     <App />
+    </Router>
+    
   </React.StrictMode>
 );
